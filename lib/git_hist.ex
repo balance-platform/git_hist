28f111b349d4bf70971918e36d1ba7f334414dae
Dmitry Shpagin <dmitry.shpagin@balance-pl.ru>
2021-02-08 09:13:09 +0300

    Initial

---
+++ lib/git_hist.ex
@@ -0,0 +1,18 @@
+defmodule GitHist do
+  @moduledoc """
+  Documentation for `GitHist`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GitHist.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/git_hist_test.exs
@@ -0,0 +1,8 @@
+defmodule GitHistTest do
+  use ExUnit.Case
+  doctest GitHist
+
+  test "greets the world" do
+    assert GitHist.hello() == :world
+  end
+end
